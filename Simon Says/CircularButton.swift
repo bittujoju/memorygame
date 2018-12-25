5f2e37de86e42c88f4385cd252d1e79432a24416
Bittu Joju <bittujoju@Bittus-MacBook-Pro.local>
2018-12-24 21:40:47 -0700

    final commit

--- Simon Says/CircularButton.swift
+++ Simon Says/CircularButton.swift
@@ -9,13 +9,19 @@
 import UIKit
 
 class CircularButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+  
+    override func awakeFromNib() {
+        layer.cornerRadius = frame.size.width / 1.8
+        layer.masksToBounds = true
     }
-    */
 
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                alpha = 1.0
+            } else {
+                alpha = 0.5
+            }
+        }
+    }
 }
