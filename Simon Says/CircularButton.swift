0856da8bbc53cc20016de0e7de135a4758498c81
Bittu Joju <bittujoju@Bittus-MacBook-Pro.local>
2018-12-24 20:22:58 -0700

    Stupid Memory Game final

---
+++ Simon Says/CircularButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircularButton.swift
+//  Simon Says
+//
+//  Created by Bittu Joju on 12/24/18.
+//  Copyright © 2018 Bittu Joju. All rights reserved.
+//
+
+import UIKit
+
+class CircularButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
